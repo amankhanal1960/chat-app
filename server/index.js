8e938a9dee552603909e83635b265e9dd6c992b6
amankhanal1960 <amankhanal1960@gmail.com>
2025-10-14 12:31:32 +0545

    ::fix:

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express!" });
 });
 
 app.listen(port, () => {
